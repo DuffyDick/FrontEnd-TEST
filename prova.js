99032c75d61b23691fd4b586e2106d486b36a38d
DuffyDick <antonio.renzullo1568@gmail.com>
2021-01-16 00:32:12 +0100

    commit

--- prova.js
+++ prova.js
@@ -1,2 +1,3 @@
-var a = "pippo";
-console.log("topolino e "+a);
+"use strict" //indica al compilatore di dare errore se le variabili non vengono inzializzate
+var a = "pippo"; //con var non si dichiara il tipo della variabile, che potra' cambiare di volta in volta
+console.log("topolino e " + a); //concatena due stringhe
